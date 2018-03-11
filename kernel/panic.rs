41a12925ba45732dd7dd161b774808f1abf478d9
Sakarah <sakarah92@gmail.com>
2018-03-11 22:39:12 +0100

    Patch kernel panic LED pattern

--- kernel/panic.rs
+++ kernel/panic.rs
@@ -14,13 +14,14 @@ pub extern fn panic_fmt(msg: fmt::Arguments, file: &'static str,
 
     gpio::select_pin_function(47, gpio::PinFunction::Output);
     gpio::select_pin_function(35, gpio::PinFunction::Output);
+
     loop
     {
+        gpio::set_pin(35);
+        gpio::clear_pin(47);
+        mmio::delay(1_000_000);
+        gpio::clear_pin(35);
         gpio::set_pin(47);
-        //gpio::clear_pin(35);
-        mmio::delay(0x100000);
-        gpio::set_pin(47);
-        //gpio::clear_pin(35);
-        mmio::delay(0x100000);
+        mmio::delay(1_000_000);
     }
 }
